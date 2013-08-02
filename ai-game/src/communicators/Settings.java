ecc2052ffe428f011fd36eaaae347ab123d916e9
Supun Nakandala <supun.nakandala@gmail.com>
2013-08-02 15:38:40 +0530

    added class Settings.java

---
+++ ai-game/src/communicators/Settings.java
@@ -0,0 +1,12 @@
+package communicators;
+/**
+ * Configurations For the Communicators
+ * Set by settings UI and used by the Sender Socket and Receiver Socket
+ * 
+ * @author Supun Nakandala
+ */
+public class Settings {
+    public static String serverIp = "localhost";
+    public static String senderPort = "6000";
+    public static String recieverPort = "7000";
+}
